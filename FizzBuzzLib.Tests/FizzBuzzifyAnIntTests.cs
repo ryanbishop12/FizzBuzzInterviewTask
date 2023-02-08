63363f795feb00b94dece43b242af721c5788e91
thatpersoninthemirror <ryan.bishop258@gmail.com>
2023-02-08 15:42:03 -0600

    fizzBuzzifyAnInt

--- FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
+++ FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
@@ -8,7 +8,66 @@ namespace FizzBuzzLib.Tests
         [Fact]
         public void FizzBuzzifyAnInt_WhenPassedAndInt_ReturnsAString()
         {
-            // test here
+            // arrange
+            var fizzBuzzClass = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzClass.FizzBuzzifyAnInt(1);
+
+            //assert
+            output.GetType().Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_WhenPassedAndInt_ReturnsNumberAString()
+        {
+            // arrange
+            var fizzBuzzClass = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzClass.FizzBuzzifyAnInt(1);
+
+            //assert
+            output.Should().Be("1");
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_WhenPassedAThree_ReturnsFizz()
+        {
+            // arrange
+            var fizzBuzzClass = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzClass.FizzBuzzifyAnInt(3);
+
+            //assert
+            output.Should().Be("Fizz");
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_WhenPassedAFive_ReturnsBuzz()
+        {
+            // arrange
+            var fizzBuzzClass = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzClass.FizzBuzzifyAnInt(5);
+
+            //assert
+            output.Should().Be("Buzz");
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_WhenPassedAFifteen_ReturnsFizzBuzz()
+        {
+            // arrange
+            var fizzBuzzClass = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzClass.FizzBuzzifyAnInt(15);
+
+            //assert
+            output.Should().Be("FizzBuzz");
         }
     }
 }
--- FizzBuzzLib/FizzBuzz.cs
+++ FizzBuzzLib/FizzBuzz.cs
@@ -1,12 +1,29 @@
 ﻿namespace FizzBuzzLib
 {
-    public class FizzBuzz //: IFizzBuzz
+    public class FizzBuzz : IFizzBuzz
     {
         public string DemoMethod()
         {
             // Feel free to delete this method
 
             return "Hello World";
+        }
+
+        public string FizzBuzzifyAnInt(int input)
+        {
+            if(input % 3 == 0 && input % 5 == 0)
+            {
+                return "FizzBuzz";
+            } else if(input % 3 == 0)
+            {
+                return "Fizz";
+            } else if(input % 5 == 0)
+            {
+                return "Buzz";
+            } else
+            {
+                return input.ToString();
+            }
         }
     }
 }
